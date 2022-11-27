9fbebba7e620687724a0867d5ecc56223b90c5df
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-11-27 21:02:27 +0000

    Adicionar exercícios resolvidos da aula 8

---
+++ Aula08/Exercicio5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace Exercicio5
+{
+    /// <summary>
+    /// Resolução do exercício 5 da aula.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Nome do ficheiro a ler.
+        /// </summary>
+        private const string FileName = "test.txt";
+
+        /// <summary>
+        /// URL do ficheiro de texto a descarregar da web.
+        /// </summary>
+        private const string TxtUrl = "http://textfiles.com/stories/roger1.txt";
+
+        /// <summary>
+        /// O programa começa aqui.
+        /// </summary>
+        private static void Main()
+        {
+            // Linhas lidas do ficheiro
+            string[] lines;
+
+            // Resultados das queries
+            int numLinesGt30;
+            double avgCharsInLines;
+            bool anyWithMoreThan120Chars;
+            IEnumerable<string> firstWordOfLineWithCharY;
+
+            // Se o ficheiro não tiver ainda sido descarregado da web, vamos
+            // descarrega-lo
+            if (!File.Exists(FileName))
+            {
+                // O using serve para fechar a ligação HTTP após descarregarmos
+                // o ficheiro
+                using HttpClient client = new HttpClient();
+                string fileText = client.GetStringAsync(TxtUrl).Result;
+                File.WriteAllText(FileName, fileText);
+            }
+
+            // Ler todas as linhas de ficheiro descarregado da web
+            lines = File.ReadAllLines(FileName);
+
+            // Indicar quantas linhas têm comprimento maior que 30 caracteres
+            numLinesGt30 =
+                (from line in lines
+                where line.Length > 30
+                select line ).Count();
+            Console.WriteLine($"Linhas > 30     : {numLinesGt30}");
+
+            // Indicar a média do nº de caracteres das linhas lidas
+            avgCharsInLines =
+                (from line in lines
+                select line.Length).Average();
+            Console.WriteLine($"Média nº chars  : {avgCharsInLines:f}");
+
+            // Indicar se existe alguma linha com mais de 120 caraceteres
+            anyWithMoreThan120Chars =
+                (from line in lines
+                where line.Length > 120
+                select line).Any();
+            Console.WriteLine($"Algum com +120? : {anyWithMoreThan120Chars}");
+
+            // Obter primeira palavra em maiúsculas das linhas que
+            // contêm o carácter 'Y'
+            firstWordOfLineWithCharY =
+                from line in lines
+                where line.Contains("Y")
+                select line.Trim().Split()[0].ToUpper();
+
+            // Mostrar primeira palavra em maiúsculas das linhas que
+            // contêm o carácter 'Y'
+            Console.WriteLine(
+                "Primeira palavra em maiúsculas de linhas com 'Y'");
+            foreach (string s in firstWordOfLineWithCharY)
+            {
+                Console.WriteLine($"=> {s}");
+            }
+        }
+    }
+}
